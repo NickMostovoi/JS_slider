fa7a0c714d45fd9c3ef3b3f0304791aefa1e2835
Mykola Mostovoi <nick211996@gmail.com>
2021-06-26 21:19:56 +0300

    исправление ошибки наведения на слайдер js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,17 +1,7 @@
-/*
- * @description
- *          Script creates a slide-show for structure .carousel>.slides>.slide[style=background-image:url()].
- * @author  Andrii.A.Fomenko
- * @revised 2021-05-11
- */
 
-/* eslint-disable no-underscore-dangle,no-unused-vars,object-property-newline */
-
-/* carousel Class */
 class Carousel {
   constructor(p) {
     let settings = { ...{ containerID: '#carousel', interval: 5000, isPlaying: true, slideID: '.slide' }, ...p };
-    // let settings = this._initConfig(params);
 
     this.container = document.querySelector(settings.containerID);
     this.slideItems = this.container.querySelectorAll(settings.slideID);
@@ -19,44 +9,6 @@ class Carousel {
     this.isPlaying = settings.isPlaying;
   }
 
-  /*
-   *
-   *_initConfig(objectParams) {
-   *
-   * Вариант 1
-   *
-   * const defaultSettings = {
-   *   containerID: '#carousel',
-   *   interval: 5000,
-   *   slideID: '.slide'
-   * };
-   *
-   * if (typeof o !== 'undefined') {
-   *   defaultSettings.containerID = objectParams.containerID || defaultSettings.containerID;
-   *   defaultSettings.interval = objectParams.interval || defaultSettings.interval;
-   *   defaultSettings.slideID = objectParams.slideID || defaultSettings.slideID;
-   *   defaultSettings.isPlaying = objectParams.isPlaying || defaultSettings.isPlaying;
-   * }
-   *
-   * return settings;
-   *
-   *
-   * Вариант 2
-   *
-   * const p = {containerID: '#carousel', interval: 5000, slideID: '.slide'};
-   *
-   * return {...p, ...o};
-   *
-   *
-   * Вариант 3
-   *
-   * return {...{containerID: '#carousel', interval: 5000, slideID: '.slide'}, ...o};
-   *
-   * }
-   *
-   */
-
-  /* private, _initProps - initialization properties */
   _initProps() {
     this.slidesCount = this.slideItems.length;
     this.currentSlide = 0;
@@ -70,7 +22,6 @@ class Carousel {
     this.FA_NEXT = '<i class="fas fa-angle-right"></i>';
   }
 
-  /* private, _initControls - dynamic creation of controls */
   _initControls() {
     let controls = document.createElement('div');
     const PAUSE = `<span id="pause-btn" class="control-pause">
@@ -95,7 +46,6 @@ class Carousel {
     this.isPlaying ? this.pauseIcon.style.opacity = 1 : this.playIcon.style.opacity = 1;
   }
 
-  /* private, _initIndicators - dynamic creation of indicators */
   _initIndicators() {
     let indicators = document.createElement('ol');
 
@@ -115,7 +65,6 @@ class Carousel {
     this.indicatorItems = this.container.querySelectorAll('.indicator');
   }
 
-  /* private, _addElemListener - adding events to the elements */
   _initListeners() {
     document.addEventListener('keydown', this._keyPress.bind(this));
     this.pauseBtn.addEventListener('click', this.pausePlay.bind(this));
@@ -126,7 +75,6 @@ class Carousel {
     this.container.addEventListener('mouseleave', this._play.bind(this));
   }
 
-  /* private, _gotoNth function */
   _gotoNth(n) {
     this.slideItems[this.currentSlide].classList.toggle('active');
     this.indicatorItems[this.currentSlide].classList.toggle('active');
@@ -135,17 +83,14 @@ class Carousel {
     this.indicatorItems[this.currentSlide].classList.toggle('active');
   }
 
-  /* private, _gotoNext function */
   _gotoNext() {
     this._gotoNth(this.currentSlide + 1);
   }
 
-  /* private, _gotoNext function */
   _gotoPrev() {
     this._gotoNth(this.currentSlide - 1);
   }
 
-  /* private, _pause function */
   _pause() {
     if (this.isPlaying) {
       this.pauseIcon.style.opacity = 0;
@@ -155,7 +100,6 @@ class Carousel {
     }
   }
 
-  /* private, _play function */
   _play() {
     if (!this.isPlaying) {
       this.pauseIcon.style.opacity = 1;
@@ -165,7 +109,6 @@ class Carousel {
     }
   }
 
-  /* private,  _indicate function */
   _indicate(e) {
     let target = e.target;
 
@@ -175,31 +118,26 @@ class Carousel {
     }
   }
 
-  /* private, _keyPress function */
   _keyPress(e) {
     if (e.code === this.CODE_LEFT_ARROW) this.prev();
     if (e.code === this.CODE_RIGHT_ARROW) this.next();
     if (e.code === this.CODE_SPACE) this.pausePlay();
   }
 
-  /* public, pausePlay function */
   pausePlay() {
     this.isPlaying ? this._pause() : this._play();
   }
 
-  /* public, next function */
   next() {
     this._pause();
     this._gotoNext();
   }
 
-  /* public, prev function */
   prev() {
     this._pause();
     this._gotoPrev();
   }
 
-  /* public, init carousel function */
   init() {
     this._initProps();
     this._initControls();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
-/* eslint-disable no-unused-vars */
 
-import Carousel from './carousel.js';
 import SwipeCarousel from './swipe-carousel.js';
 
 let carousel = new SwipeCarousel({
   containerID: '#carousel',
   interval: 2000
-  // isPlaying: false
 });
 
 carousel.init();
